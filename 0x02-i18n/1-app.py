08090d566ec5bc7c9aa29226d73c316a34a0bd1d
reinhard <reinhard2057@gmail.com>
2023-05-24 12:11:51 +0000

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-flask app
+module: flask app and babel app
 """
 from flask import Flask, render_template
 from flask_babel import Babel
@@ -15,6 +15,8 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app.config.from_object(Config)
 
 @app.route('/')
 def basic_app():
